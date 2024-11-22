00ec28b7953d7b7e60e5f2807e867b7554a0c2dc
Djebreds <refiahmad.djebreds@gmail.com>
2024-11-23 01:03:03 +0700

    chore: update and configure depedencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,43 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+const dotenv = require('dotenv');
+dotenv.config();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    dangerouslyAllowSVG: true,
+    localPatterns: [
+      {
+        pathname: '/public/assets/**',
+        search: '',
+      },
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.aceternity.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'github-readme-stats.vercel.app',
+      },
+      {
+        protocol: 'https',
+        hostname: 'leetcard.jacoblin.cool',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
