15c59cfd6af227d42ab8ba13f563073256ad556e
Djebreds <refiahmad.djebreds@gmail.com>
2024-11-23 01:16:35 +0700

    feat: create techstack data

---
+++ src/data/techstack.ts
@@ -0,0 +1,131 @@
+export const frameworks = [
+  {
+    name: 'Ruby on Rails',
+    title: 'Ruby on Rails',
+  },
+  {
+    name: 'Nest.js',
+    title: 'Nest.js',
+  },
+  {
+    name: 'Express.js',
+    title: 'Express.js',
+  },
+  {
+    name: 'Next.js',
+    title: 'Next.js',
+  },
+  {
+    name: 'Laravel',
+    title: 'Laravel',
+  },
+  {
+    name: 'Flutter',
+    title: 'Flutter',
+  },
+];
+
+export const databases = [
+  {
+    name: 'PostgreSQL',
+    title: 'PostgreSQL',
+  },
+  {
+    name: 'MySQL',
+    title: 'MySQL',
+  },
+  {
+    name: 'Redis',
+    title: 'Redis',
+  },
+  {
+    name: 'MongoDB',
+    title: 'MongoDB',
+  },
+  {
+    name: 'TypeORM',
+    title: 'TypeORM',
+  },
+  {
+    name: 'Prisma',
+    title: 'Prisma',
+  },
+];
+
+export const infrastructures = [
+  {
+    name: 'Docker',
+    title: 'Docker',
+  },
+  {
+    name: 'Nginx',
+    title: 'Nginx',
+  },
+  {
+    name: 'Kubernetes',
+    title: 'Kubernetes',
+  },
+  {
+    name: 'GIT',
+    title: 'GIT',
+  },
+  {
+    name: 'Gitlab',
+    title: 'Gitlab',
+  },
+  {
+    name: 'Github',
+    title: 'Github',
+  },
+  {
+    name: 'Digital Ocean',
+    title: 'Digital Ocean',
+  },
+  {
+    name: 'AWS',
+    title: 'AWS',
+  },
+  {
+    name: 'GCP',
+    title: 'GCP',
+  },
+  {
+    name: 'RabbitMQ',
+    title: 'RabbitMQ',
+  },
+  {
+    name: 'BullMQ',
+    title: 'BullMQ',
+  },
+  {
+    name: 'Sidekiq',
+    title: 'Sidekiq',
+  },
+];
+
+export const languages = [
+  {
+    name: 'Ruby',
+    title: 'Ruby',
+  },
+  {
+    name: 'TypeScript',
+    title: 'TypeScript',
+  },
+  {
+    name: 'PHP',
+    title: 'PHP',
+  },
+  {
+    name: 'Javascript',
+    title: 'Javascript',
+  },
+  {
+    name: 'SCSS',
+    title: 'SCSS',
+  },
+  {
+    name: 'TailwindCSS',
+    title: 'TailwindCSS',
+  },
+];
